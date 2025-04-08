740eaa693c907466f9d2b2efdf7759bda27dd003
Viraj Dey <viraj65.dey@gmail.com>
2025-04-08 16:48:01 +0530

    route fix

--- server.js
+++ server.js
@@ -19,7 +19,6 @@ const allowedOrigins = [
 
 app.use(cors({
   origin: function(origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
     if (!origin) return callback(null, true);
     
     if (allowedOrigins.indexOf(origin) === -1) {
@@ -29,7 +28,8 @@ app.use(cors({
     return callback(null, true);
   },
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: ['Content-Type', 'Authorization', 'Accept', 'Origin'],
+  exposedHeaders: ['Content-Length', 'Content-Type'],
   credentials: true
 }));
 
